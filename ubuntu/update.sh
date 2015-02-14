709f562d42ce72ba7840cf808098fb2c566a05ec
Boris LECLERE <borisleclere@gmail.com>
2015-02-14 13:04:58 +0100

    Missing space

--- ubuntu/update.sh
+++ ubuntu/update.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade
-sudo apt-get remove -y--purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
+sudo apt-get remove -y --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
 sudo apt-get autoclean -y
 sudo apt-get autoremove -y
