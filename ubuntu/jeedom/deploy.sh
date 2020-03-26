b2e086943277a1f45f5409373785aaf36692d59a
Boris LECLERE <Borisleclere@gmail.com>
2020-03-26 09:11:56 +0100

    Missing mount

--- ubuntu/jeedom/deploy.sh
+++ ubuntu/jeedom/deploy.sh
@@ -18,6 +18,8 @@ echo "Configure fstab"
 appendToFile "username=jeedom\npassword= ypi1FWY5" /home/pi/.RinzlerNasJeedomCred
 appendToFile "//RinzlerNas/home/backup /var/www/html/backup cifs vers=1.0,_netdev,users,auto,users,credentials=/home/pi/.RinzlerNasJeedomCred 0 0" /etc/fstab
 
+sudo mount -a
+
 echo "Configure network dispatcher"
 sudo apt-get install -y network-manager
 appendToFile "#!/bin/bash\nlogger \"Running $0\"\ncase \"$1\" in\n    eth*)\n        if [ \"$2\" == up ]; then		logger \"$1 $2 wifi off\"\n		nmcli radio wifi off        elif [ \"$2\" == down ]; then\n		logger \"$1 $2 wifi on\"\n		nmcli radio wifi on	\n	fi\n        ;;\nesac" /etc/NetworkManager/dispatcher.d/50wifioff
