591e07dd1b0e1c74aeb8dfecab821205cf04c9c0
Boris LECLERE <Borisleclere@gmail.com>
2020-03-26 08:42:48 +0100

    Fix syntaxe Move clean

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,3 +1,7 @@
 sudo bash ./update.sh
 sudo bash ./tools/networkmanager.config.sh
 sudo bash ./webmin/webmin.install.sh
+
+sudo apt-get clean -y
+sudo apt-get autocolean -y
+sudo apt-get autoremove -y
--- ubuntu/webmin/webmin.install.sh
+++ ubuntu/webmin/webmin.install.sh
@@ -1,9 +1,6 @@
 wget -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
 sudo echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
-sudo eho "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
+sudo echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install webmin
-sudo apt-get clean -y
-sudo apt-get autocolean -y
-sudo apt-get autoremove -y
