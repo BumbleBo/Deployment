583983126cd3fdb874a9028494f8c57c64d27a60
Boris LECLERE <Borisleclere@gmail.com>
2020-03-26 08:47:41 +0100

    Prevent from duplicate

--- ubuntu/webmin/webmin.install.sh
+++ ubuntu/webmin/webmin.install.sh
@@ -1,6 +1,17 @@
+
+function appendToFile
+{
+    text=$1
+    filePath=$2
+    
+    if ! grep -q "$1" "$2"; then
+        sudo echo "$1" >> "$2"
+    fi
+}
+
 wget -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
-sudo echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
-sudo echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
+appendToFile "deb http://download.webmin.com/download/repository sarge contrib" /etc/apt/sources.list
+appendToFile "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" /etc/apt/sources.list
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y webmin
